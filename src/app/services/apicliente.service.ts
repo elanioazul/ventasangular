81ef700550fb8f4c0d7c746062675e62f2b10d37
elanioazul_hugo <hugo.herrador@gmail.com>
2021-07-14 16:22:36 +0200

    Add service for client crud operations

--- src/app/services/apicliente.service.ts
+++ src/app/services/apicliente.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Response } from '../models/response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiclienteService {
 
-  constructor() { }
+  public url = 'https://localhost:44396/api/cliente';
+
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Response> {
+    return this.http.get<Response>(this.url)
+  }
 }
