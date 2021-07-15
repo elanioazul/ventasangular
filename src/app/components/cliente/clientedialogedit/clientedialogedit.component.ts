afdd9a60c904657bdf99e5d5c9bd72f838151ab3
elanioazul_hugo <hugo.herrador@gmail.com>
2021-07-15 15:27:35 +0200

    Finish having two different c for edit and delet dialog

--- src/app/components/cliente/clientedialogedit/clientedialogedit.component.ts
+++ src/app/components/cliente/clientedialogedit/clientedialogedit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Cliente } from 'src/app/models/cliente';
+import { ApiclienteService } from '../../../services/apicliente.service';
+import {ClienteComponent} from '../cliente.component';
 @Component({
   selector: 'app-clientedialogedit',
   templateUrl: './clientedialogedit.component.html',
@@ -8,13 +10,15 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ClientedialogeditComponent implements OnInit {
 
   @Input() indexito?: number;
-  @Input() name?: string;
+  @Input() name: string;
 
   public displayPosition?: boolean;
 
   public position?: string;
 
-  constructor() { }
+  constructor(public service: ApiclienteService, public clienteC: ClienteComponent) { 
+    this.name = '';
+  }
 
   ngOnInit(): void {
     
@@ -23,7 +27,24 @@ export class ClientedialogeditComponent implements OnInit {
   showPositionDialog(position: string) {
       this.position = position;
       this.displayPosition = true;
-      console.log('indice que llega a dialog Edit es ' + this.indexito)
+      console.log('indice que llega a dialog Edit es ' + this.indexito);
+      let c: Cliente = { id: this.indexito, nombre: this.name};
+      this.service.editCliente(c).subscribe( res => {
+        if (res.exito === 1) {
+          window.alert('cliente editado')
+      }
+      this.clienteC.obtainClientes();
+    })
+  }
+
+  editClient() {
+    let c: Cliente = { id: this.indexito, nombre: this.name};
+    this.service.editCliente(c).subscribe( res => {
+      if (res.exito === 1) {
+        window.alert('cliente editado')
+      }
+      this.clienteC.obtainClientes();
+    })
   }
 
 }
